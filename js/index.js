6e999ccedde609ea0c6deef4a8ca25b70ffa5626
h3poteto <h3.poteto@gmail.com>
2013-12-05 23:39:38 +0900

    add christmas

--- js/index.js
+++ js/index.js
@@ -2,5 +2,15 @@ $(function(){
   setTimeout(function(){
     $('#alert_side').fadeIn(500);
   }, 2000);
+  
+  $(".modal_link").click(function(){
+    $("#modalback").show();
+    $("#modalwindow").show();
+    $("#modalback").height($("#wrap").height() + 50);
+  });
+  $("#modalback").click(function(){
+    $("#modalback").hide();
+    $("#modalwidnow").hide();
+  });
 
 });
